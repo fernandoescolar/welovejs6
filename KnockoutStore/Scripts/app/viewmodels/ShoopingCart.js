fb953d851925f8d9d1aad601aa1fcb74afcd1ea1
Marckys <lonetcamp@hotmail.com>
2013-10-24 15:20:57 +0200

    Validaciones

---
+++ KnockoutStore/Scripts/app/viewmodels/ShoopingCart.js
@@ -0,0 +1,22 @@
+﻿
+
+var ShoopingCartMV = function() {
+
+    return {        
+        nombre : ko.observable("Marc").extend({ 
+            required: true,
+            minLength: 3,
+            pattern: {
+                message: 'Hey this doesnt match my pattern',
+                params: '^[A-Z0-9].$'
+            }
+        })
+    };
+
+};
+
+$(function() {
+    ko.applyBindings(new ShoopingCartMV());
+});
+
+
