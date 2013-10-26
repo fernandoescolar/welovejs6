a9df18fb792b8a27d9cb25093e4f3d88bb0d843a
Marckys <lonetcamp@hotmail.com>
2013-10-26 08:50:17 +0200

    User Info

---
+++ KnockoutStore/Scripts/app/viewmodels/UserInfo.js
@@ -0,0 +1,22 @@
+﻿
+'use strict';
+
+
+var UserInfo = function () {
+
+    var sef = {        
+        email: ko.observable().extend({ 
+            required: true,
+            minLength: 3
+        }),
+        password: ko.observable(),
+        condiciones: ko.observable(false)
+    };
+
+
+    return self;
+
+};
+
+
+ko.applyBindings(new UserInfo(), document.getElementById("UserForm"));
