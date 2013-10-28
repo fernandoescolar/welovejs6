8c4e8690f32097d9b5e98f67d1f284845e14b1d9
fernandoescolar <fer.escolar@gmail.com>
2013-10-28 08:35:16 +0100

    added the filter in the index page

--- KnockoutStore/Scripts/app/viewmodels/ProductList.js
+++ KnockoutStore/Scripts/app/viewmodels/ProductList.js
@@ -3,13 +3,27 @@
 var ProductListViewModel = (function () {
 
     var isVisible = ko.observable(true);
+    var filter = ko.observable();
     var products = ko.observableArray([]);
     var storeService;
     var notifier;
+    var allProducts;
 
+    filter.subscribe(function(value) {
+        var result = [];
+        for (var i = 0; i < allProducts.length; i++) {
+            if (allProducts[i].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+                result.push(allProducts[i]);
+            }
+        }
+
+        products(result);
+    });
+    
     function loadData() {
         storeService.getAllItems().then(function (data) {
-            products(data);
+            allProducts = data;
+            products(allProducts);
         });
     }
 
@@ -30,6 +44,7 @@ var ProductListViewModel = (function () {
 
         return {
             isVisible: isVisible,
+            filter: filter,
             products: products,
             select: select
         };
